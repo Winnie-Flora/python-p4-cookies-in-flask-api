540f6a7ba9e29a24b62032964acad18aaebe3677
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-10-29 22:09:47 +0300

    Cookies Flask Api

--- server/app.py
+++ server/app.py
@@ -1,30 +1,55 @@
-from flask import Flask, request, session, jsonify, make_response
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, make_response, session
+from flask_migrate import Migrate
+from models import Article, User, db
 
 app = Flask(__name__)
-app.json.compact = False
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
+db.init_app(app)
+migrate = Migrate(app, db)
 
-@app.route('/sessions/<string:key>', methods=['GET'])
-def show_session(key):
 
-    session["hello"] = session.get("hello") or "World"
-    session["goodnight"] = session.get("goodnight") or "Moon"
+@app.route('/clear')
+def clear_session():
+    session['page_views'] = 0
 
-    response = make_response(jsonify({
-        'session': {
-            'session_key': key,
-            'session_value': session[key],
-            'session_accessed': session.accessed,
-        },
-        'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
-    }), 200)
+    return {'message': '200: Successfully cleared session data.'}, 200
 
-    response.set_cookie('mouse', 'Cookie')
 
-    return response
+@app.route('/articles')
+def index_articles():
+    article_list = Article.query.all()
+
+    article_dict_list = [
+        article.to_dict(rules=('-content', )) for article in article_list
+    ]
+
+    return article_dict_list
+
+
+@app.route('/articles/<int:id>')
+def show_article(id):
+    if 'page_views' not in session:
+        session['page_views'] = 0
+
+    print(session['page_views'])
+
+    if session['page_views'] < 3:
+        article = db.session.get(Article, id)
+
+        session['page_views'] += 1
+
+        return jsonify(article.to_dict())
+
+    else:
+        session['page_views'] += 1
+
+        return jsonify(message='Maximum pageview limit reached'), 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
-    
